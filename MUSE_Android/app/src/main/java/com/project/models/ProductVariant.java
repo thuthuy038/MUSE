b16c7d35d26bf01abad20045db0be21e8785a4d2
thuyhtt038 <thuyhtt23411@st.uel.edu>
2026-06-29 16:01:52 +0700

    Add reusable vertical product card component

---
+++ MUSE_Android/app/src/main/java/com/project/models/ProductVariant.java
@@ -0,0 +1,34 @@
+package com.project.models;
+
+public class ProductVariant {
+
+    private String size;
+    private String color;
+    private int quantity;
+
+    // getter setter
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
